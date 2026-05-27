87d2f714583d818d86c02157a0704274fae78f26
Ville-Petteri Koivunen <vp.koivunen@hotmail.fi>
2026-05-27 12:27:45 +0300

    Add project files.

---
+++ Tdd_With_Interfaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace Tdd_With_Interfaces
+{
+    public interface IClass1
+    {
+        string Name { get; }
+        //string TypeName { get; }
+        //string FullName { get; }
+        //string FullTypeName { get; }
+
+
+    }
+}
