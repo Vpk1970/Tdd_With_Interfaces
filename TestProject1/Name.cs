9fe66bc4eb9b4284e15c55e2303ef323dc5dec23
Ville-Petteri Koivunen <vp.koivunen@hotmail.fi>
2026-06-10 08:17:01 +0300

    korjausta

--- TestProject1/Name.cs
+++ TestProject1/Name.cs
@@ -4,19 +4,15 @@ namespace TestProject1
 {
     internal class Name<T> : IName<T>
     {
-        public Name(string nameValue) 
-        {
-            if (string.IsNullOrEmpty(nameValue))
-            {
-                Value = "Empty";
-            }
-            else
-            {
-                Value = nameValue;
-            }
+        private string _name = string.Empty;
+                
+        public Name(string name) { _name = name; }
+
+        string IName<T>.Name 
+        { 
+            get { return _name; }
+            set { _name = value; } 
         }
 
-        public string Value { get; set; } = string.Empty; // ""
-        string IName<T>.Name { get => Value; set => Value = value; }
     }
 }
